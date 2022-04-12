4b1c8d8185384db03426f1e038c6a7cd8df7c909
aligredo <aliahmedismail37@gmail.com>
2022-04-12 12:38:07 +0200

    maven project initialization

---
+++ src/main/java/com/example/javaegypitanidinfoextractor/JavaEgypitanIdInfoExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaegypitanidinfoextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEgypitanIdInfoExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEgypitanIdInfoExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaegypitanidinfoextractor/JavaEgypitanIdInfoExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaegypitanidinfoextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEgypitanIdInfoExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
